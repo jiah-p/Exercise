504531e9a3c60f73546f040ee3aa05b775e4dbce
pjh <jiah_pan@163.com>
2024-06-07 18:17:24 +0800

    24.06.07

--- test/exercise.py
+++ test/exercise.py
@@ -2,26 +2,20 @@
 # 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
 #
 # 
-# @param arr int整型一维数组 the array
+# @param areas int整型一维数组 
 # @return int整型
 #
 class Solution:
-    def maxLength(self , arr) -> int:
+    def maxArea(self , areas) -> int:
         # write code here
-        # viw = set()
-        temp = []
-        res = []
-        for i in arr:
-            if i not in temp:
-                temp.append(i)
-            else:
-                res.append(temp.copy())
-                index = temp.index(i)
-                temp = temp[index:]
-        if temp:
-            res.append(temp.copy())
-        maxi = max([len(i) for i in res])
-        return maxi
+        res = 0
+        temp = 0
+        for i in range(len(areas)):
+            res = max(min(areas[temp:i+1]) * (i-temp +1), res, areas[i])
+            if res == areas[i]:temp = i
+        return res
+    
+
 s = Solution()
-res = s.maxLength([2,2,3,4,3])
+res = s.maxArea([1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1])
 print(res)
